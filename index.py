b24cf7d9a8d51e95e010c3e98fabb30eed7666a7
Jessica Susilo <jxsusilo@gmail.com>
2022-11-18 22:34:31 -0800

    Made indexing more efficient and adjusted search to work with this new index
    Indexing was taking way too long, so we needed to do it faster.

--- index.py
+++ index.py
@@ -6,6 +6,9 @@ import shelve
 import json
 from bs4 import BeautifulSoup
 import re
+from collections import defaultdict
+
+main_index = defaultdict(lambda: defaultdict(int)) #stores index in the format {token: {docid: freq}}
 
 def get_batch(D, batch_num, total_batches):
     # Split total number of documents into 3 
@@ -18,89 +21,56 @@ def get_batch(D, batch_num, total_batches):
     start_i_list.append(len(D))
 
     for i in range(start_i_list[batch_num-1], start_i_list[batch_num]):
-        #print(i)
-        #print(D[i])
         batch.append(D[i])
 
-    #start_i = (batch_num - 1) * len(D) // total_batches
-
-    #batch 1: i = 0 to len(D) // 3
-    '''
-    if batch_num == 1:
-        i = 0
-        for i in range(len(D) // 3):
-            if x ==True:
-                print(i)
-                x = False
-            batch.append(D[i])
-    elif batch_num == 2:
-        # i = batch_num // 3
-        for i in range(len(D) // 3, int(len(D) * (2.0/3.0))):
-            if x ==True:
-                print(i)
-                x = False
-            batch.append(D[i])         
-    elif batch_num == 3:
-        # i = int(len(D) * (2.0/3.0))
-        for i in range(int(len(D) * (2.0/3.0)), len(D)):
-            if x ==True:
-                print(i)
-                x = False
-            batch.append(D[i])         
-    
-    '''
     print('batch len', len(batch), batch[0], batch[(len(batch)-1)])
     return batch
 
-def parse(document):
-    #Opens and parses through the document and returns a list of its tokens
-    #print_tokens = False
-    #if document == '../DEV/grape_ics_uci_edu/288efb3cf695c4647ffa91355363552cdc0d45fc7dd93fbd1804b8e8db25dcba.json':
-        #print('TEST DOC')
-        #print_tokens=True
+def tokenize(text, docid):
+    '''
+    Tokenizes and adds the tokens to the main_index
+    '''
+    ps = PorterStemmer()
+    for line in text:
+        try:
+            words = re.sub(r'[^a-zA-Z0-9]+', ' ', line)
+            words = words.split()
+            for word in words:
+                word = ps.stem(word.lower())
+                main_index[word][docid] += 1
+        except:
+            pass
+
+def get_text_content(document):
+    '''
+    Gets the text content from a json file
+    '''
+    text_content = ''
+
     try:
         f = open(document)
         text = f.read()
+        f.close()
         text = json.loads(text)
         soup = BeautifulSoup(text['content'], features="html.parser")
-
         text_content = soup.get_text()
+    except:
+        print('Failed to get content from', document)
 
-        # Stemming
-        ps = PorterStemmer()
+    return text_content
 
-        res = []
 
-        #test
-        print('parsing', document)
-        for line in text_content.split('\n'):
-            for word in line.split():
-                #if print_tokens == True:
-                    #print(word)
-                if bool(re.match('^[a-zA-Z0-9]+$', word)): # if the word is alphanumeric
-                    #res.append(ps.stem(word).lower())
-                    res.append(word.lower())
-                else:
-                    addTokens = [] # stores the words that we must add
-                    currWord = []
-                    for c in word:
-                        if not bool(re.match('^[a-zA-Z0-9]$', c)): # if not alphanumeric char, we will reset the currWord = ""
-                            if currWord != []: 
-                                addTokens.append("".join(currWord)) # we only want to append currWord if it's not ""
-                                currWord = [] 
-                        else:
-                            currWord.append(c.lower())
+def parse(document, docid):
+    '''
+    Opens and parses the document
+    '''
 
-                    if currWord != []: # if after we finish looping through the whole word and there's a word we haven't added
-                        addTokens.append("".join(currWord)) # we add that word that we haven't added yet
-                    res += addTokens # then we add all these words to the result!
+    text_content = get_text_content(document)
+    text_content = text_content.split('\n')
 
-        f.close()
-        return res
-        
-    except (OSError, IOError) as e:
-        print('error in parsing')
-        return []
+    #test
+    print('parsing', docid, document)
+    tokenize(text_content, docid)
     
 
 def remove_duplicates(tokens):
@@ -108,24 +78,28 @@ def remove_duplicates(tokens):
     no_duplicates = list(set(tokens))
     return no_duplicates
 
-def sort_and_write_to_disk(index, name):
+def sort_and_write_to_disk(name):
+    '''
+    writes to file and clears the main_index
+    '''
 
     print('writing to file')
 
     try:
         inv_index = shelve.open(name)
-        for i in index.keys():
+        for i in main_index.keys():
             if i in inv_index:
                 temp = inv_index[i]
-                temp.extend(index[i])
+                temp.update(main_index[i])
                 inv_index[i] = temp
             else:
-                inv_index[i] = index[i]
+                inv_index[i] = main_index[i]
         inv_index.close()
         
     except:
         print('could not open file')
 
+    main_index.clear()
 
 def calculate_tfidf(D, token, tokens):
     # tf-idf = term frequency of a word in a document
@@ -135,11 +109,14 @@ def calculate_tfidf(D, token, tokens):
 
 
 def build_index(D):                           # D = set of text documents
-    index = {}          # Inverted list storage
+    '''
+    Builds an inverted index on main_index.
+    Goes through documents in D in separate batches,
+    parsing and tokenizing each one.
+    Writes the inverted index to file after every batch.
+    '''
     n = 0               # Document numbering
     b = []              # Batch of documents
-    num_indexed = 0
-    #total_unique_tokens = 0
     batch_num = 1
     total_batches = 3
     #inverted_index_file = 'inv_index'
@@ -147,29 +124,16 @@ def build_index(D):                           # D = set of text documents
     
     while batch_num <= total_batches:  # While D is not empty
         b = get_batch(D, batch_num, total_batches)
+        x = input()
         print('batch num', batch_num)
 
-        print('LEN(b)=', len(b), b[len(b)-1])
-        
         for i in range(len(b)):
             document = b[i]
-            #print('on document no.', i)
-            tokens = parse(document)                # Tokenize document in current batch
-            #if tokens == []:
-                #n += 1                              # Make sure to still skip the one we couldn't open
-                #break
-            unique_tokens = remove_duplicates(tokens)
-            for token in unique_tokens:
-                if token not in index:
-                    index[token] = [] 
-                index[token].append(Posting(n, calculate_tfidf(D, token, tokens)))
+            parse(document, n)
             n += 1
-            num_indexed += 1
-        #total_unique_tokens += len(index)
-        # print(index.keys())
-        sort_and_write_to_disk(index, inverted_index_file)
-        index = {}
-        
+
+        sort_and_write_to_disk(inverted_index_file)
+
         batch_num += 1
 
-    return num_indexed #, total_unique_tokens
+    return n #, total_unique_tokens
--- search.py
+++ search.py
@@ -4,62 +4,51 @@ from collections import defaultdict
 import time
 import json
 
+FILENAME = 'inv_index_dev'
+
 def get_query():
     '''
     Get the query from user input
+    and lowercases+stems it
     '''
+    ps = PorterStemmer()
     query = input('Enter your search query: ')
     query = query.split()
+    query = [ps.stem(q.lower()) for q in query]
     return query
 
 def get_top_docids(query):
     '''
     Find the documents that fulfill an AND query,
     sorted by relevance
+    (For new indexer!)
     '''
-    docs_scores = dict()
-    #postings_lists = []
-    doc_freq = defaultdict(dict)
+    docs_scores = defaultdict(int)
     common_docids = set()
 
-    filename = 'inv_index' 
-    inv_index = shelve.open(filename)
+    inv_index = shelve.open(FILENAME)
 
-    #get the Postings list for each word in query
-    for word in query:
-        word_docids = set()
-        if word in inv_index:
-            postings = inv_index[word]
-            for p in postings:
-                doc_freq[word][p.get_docid()] = p.get_tfidf()
-                word_docids.add(p.get_docid())
-        else:
-            return [] #return empty list if any of the words does not exist in the inverted index
-        if len(common_docids) == 0:
-            common_docids = word_docids
-        else:
-            common_docids = common_docids.intersection(word_docids)
-
+    if len(query) != 0:
+        try:
+            common_docids = set(inv_index[query[0]].keys())
+            for i in range(1, len(query)):
+                common_docids = common_docids.intersection(set(inv_index[query[i]].keys()))
+            for id in common_docids:
+                for q in query:
+                    docs_scores[id] += (inv_index[q][id])
+        except:
+            print('Query not found in index')
+    
     inv_index.close()
 
-    #for every Posting:
-    #if the doc appears for all words in the query, then calculate the score and store it
-    for docid in common_docids:
-        score = 0
-        for word in query:
-            #TODO make sure this works ok for calculating score, maybe use cosine similarity?
-            score += doc_freq[word][docid]
-        docs_scores[docid] = score
+    sorted_docs = list(docs_scores.items())
+    sorted_docs.sort(key=lambda x: x[1], reverse=True)
+    sorted_docs = [id for id, _ in sorted_docs]
 
-    #sort the documents by score in descending order
-    docs = list(docs_scores.items())
-    docs.sort(key=lambda x: x[1], reverse=True)
-    docs = [id for id, _ in docs]
-
-    return docs
+    return sorted_docs
 
 def display_top_n_urls(docids, num_to_display):
-    docids_map = shelve.open('docids')
+    docids_map = shelve.open('docids_dev')
     for i in range(num_to_display):
         try:
             f = open(docids_map[str(docids[i])])
@@ -71,33 +60,14 @@ def display_top_n_urls(docids, num_to_display):
             pass
     docids_map.close()
 
-def find_docs(query):
-    '''
-    Find the documents that fulfill an AND query
-    (OLD)
-    '''
-    #ps = PorterStemmer()
-    filename = 'inv_index' 
-    inv_index = shelve.open(filename)
-    set_of_docs = set() #Postings
-    first_word = True
-    for word in query:
-        print(word)
-        if word in inv_index:
-            print('found')
-            #word = word.lower()
-            docs_list = inv_index[word]
-            for x in docs_list:
-
-                print(x.get_tfidf())
-            docs_list = set([d.docid for d in docs_list])
-            if first_word == True:
-                set_of_docs = set(docs_list)
-                first_word = False
-            else:
-                set_of_docs = set_of_docs.intersection(docs_list)
-            
-    return set_of_docs
+def display_top_n_files(docids, num_to_display):
+    docids_map = shelve.open('docids_dev')
+    for i in range(num_to_display):
+        try:
+            print(docids_map[str(docids[i])])
+        except:
+            pass
+    docids_map.close()
 
 def execute():
     query = get_query()
@@ -107,6 +77,7 @@ def execute():
 
     set_of_docs = get_top_docids(query)
     display_top_n_urls(set_of_docs, 5)
+    #display_top_n_files(set_of_docs, 5)
 
     end_time = time.perf_counter()
     print(f'{end_time-start_time:0.4f} seconds elapsed')
